57392af44d0a588eb759eda91e778a7e8947b4d7
Jirka Borovec <jirka.borovec@seznam.cz>
2020-12-06 17:26:50 +0100

    add intermediate setters

--- pytorch_lightning/accelerators/accelerator.py
+++ pytorch_lightning/accelerators/accelerator.py
@@ -245,14 +245,4 @@ class Accelerator(object):
         return checkpoint
 
 
-# TODO: allow user to compare with string even internaly we shall use these Enum to prevent typos...
-class BackendType(Enum):
-    DP = 'dp'
-    DDP = 'ddp'
-    DDP2 = 'ddp2'
-    DDP_SPAWN = 'ddp_spawn'
-    # decuple distrib and device
-    DDP_CPU = 'ddp_cpu'
-    HOROVOD = 'horovod'
-    # this is rather device
-    TPU = 'tpu'
+
