7c436619906c73b760ce9105883468a61b192bac
sid-sundrani <sidhant96@outlook.com>
2020-12-11 19:17:18 +0530

    edit every_n_epoch to every_n_epochs

--- tests/core/test_datamodules.py
+++ tests/core/test_datamodules.py
@@ -452,6 +452,6 @@ def test_dm_reload_dataloaders_every_epoch(tmpdir):
         default_root_dir=tmpdir,
         max_epochs=2,
         limit_train_batches=0.01,
-        reload_dataloaders_every_n_epoch=True,
+        reload_dataloaders_every_n_epochs=True,
     )
     trainer.fit(model, dm)
