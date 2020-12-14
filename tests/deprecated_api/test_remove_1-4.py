1029015c0bb8c088c797c81372f49b3154c691db
sid-sundrani <sidhant96@outlook.com>
2020-12-15 03:32:12 +0530

    remove unnecessary code in deprec test

--- tests/deprecated_api/test_remove_1-4.py
+++ tests/deprecated_api/test_remove_1-4.py
@@ -26,8 +26,6 @@ from tests.base.boring_model import BoringModel
 def test_reload_dataloaders_every_epoch_remove_in_v1_4_0(tmpdir):
 
     with pytest.deprecated_call(match='will be removed in v1.4'):
-        model = BoringModel()
-
         trainer = Trainer(
             default_root_dir=tmpdir,
             limit_train_batches=0.3,
@@ -35,20 +33,3 @@ def test_reload_dataloaders_every_epoch_remove_in_v1_4_0(tmpdir):
             reload_dataloaders_every_epoch=True,
             max_epochs=3,
         )
-        trainer.fit(model)
-        trainer.test()
-
-        # verify the sequence
-        calls = trainer.dev_debugger.dataloader_sequence_calls
-        expected_sequence = [
-            'val_dataloader',
-            'train_dataloader',
-            'val_dataloader',
-            'train_dataloader',
-            'val_dataloader',
-            'train_dataloader',
-            'val_dataloader',
-            'test_dataloader'
-        ]
-        for call, expected in zip(calls, expected_sequence):
-            assert call['name'] == expected
